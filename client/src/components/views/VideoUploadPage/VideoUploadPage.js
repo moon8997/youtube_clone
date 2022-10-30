cf5d187b8bc43094eeee82b3b6c20af1630999fe
moon8997 <tjdansrlf@gmail.com>
2022-10-30 22:04:40 +0900

    ffmpeg로 썸네일 생성

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -26,6 +26,11 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
+
+
 
     const onTitleChange = (e) => {
         // console.log(e.currentTarget)
@@ -57,6 +62,27 @@ function VideoUploadPage() {
             .then(response => {
                 if(response.data.success) {
                     console.log(response.data)
+
+                    let variable = {
+                        url:response.data.url,
+                        fileName: response.data.fileName
+                    }
+
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                    .then(response => {
+                        if(response.data.success) {
+                            // console.log(response.data)
+
+                            setDuration(response.data.fileDuration)
+                            setThumbnailPath(response.data.url)
+
+                        } else {
+                            alert('썸네일 생성에 실패했습니다.')
+                        }
+                    })
+
                 } else{
                     alert('비디오 업로드를 실패했습니다.')
                 }
@@ -89,10 +115,16 @@ function VideoUploadPage() {
                      )}
                  </Dropzone>
 
-                    {/* Thumbnail */}
-                    <div>
-                        <img src alt />
-                    </div>
+                    {/*Thumbnail*/}
+
+                    {ThumbnailPath &&
+                        <div>
+
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="Thumbnail" />
+
+                        </div>
+                    }
+                        
                 </div>
             
                 <br />
